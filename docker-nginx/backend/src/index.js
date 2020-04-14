768580fb01d444672ce149ef5225535369ab918d
thanit456 <too_w_o@hotmail.com>
2020-04-14 14:42:00 +0700

    load balancer without integration

---
+++ docker-nginx/backend/src/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    console.log('I just received a Get request on port 3000!');
+    res.send('Hello World!');
+})
+
+app.listen(3000, () => {
+    console.log('I just connected on port 3000!');
+})
