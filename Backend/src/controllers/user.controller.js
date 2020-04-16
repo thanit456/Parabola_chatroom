e214a2e5268b14fd9b61879793d0cdcf49057081
Pawat Amonpitakpun <p.amonpitakpun@gmail.com>
2020-04-17 00:38:34 +0700

    enforce linter

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -4,33 +4,33 @@ import User from '../models/user.model.js';
 
 export default class UserController {
 
-    static async createUser(req, res){
-        const userExist = await User.exists( { username: req.body.username });
-        if(userExist){
-            throw new HttpErrors.BadRequest();
-        }
-        else{
-            const new_user = new User({
-                username: req.body.username,
-                password: req.body.password,
-                createTime: new Date()
-            });
-            await new_user.save();
-            res.json({ status: 'success' });
-        }
-    }
+	static async createUser(req, res){
+		const userExist = await User.exists( { username: req.body.username });
+		if(userExist){
+			throw new HttpErrors.BadRequest();
+		}
+		else{
+			const new_user = new User({
+				username: req.body.username,
+				password: req.body.password,
+				createTime: new Date()
+			});
+			await new_user.save();
+			res.json({ status: 'success' });
+		}
+	}
 
-    static ensureLoggedIn(req, res, next){ 
-        if(req.user){
-            next();
-        }else{
-            throw new HttpErrors.Unauthorized();
-        }
+	static ensureLoggedIn(req, res, next){ 
+		if(req.user){
+			next();
+		}else{
+			throw new HttpErrors.Unauthorized();
+		}
     
-    }
+	}
 
-    static whoami(req, res){
-        res.json(req.user);
-    }
+	static whoami(req, res){
+		res.json(req.user);
+	}
 
 }
